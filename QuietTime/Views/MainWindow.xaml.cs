855bf0a5a9efc4636e931dceab13bfde3275319c
Jack Kendall <jkendall3096@gmail.com>
2021-06-20 12:51:55 +0100

    Add basic system tray support

--- QuietTime/Views/MainWindow.xaml.cs
+++ QuietTime/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using QuietTime.ViewModels;
+using System.ComponentModel;
 using System.Windows;
 
 namespace QuietTime
@@ -13,6 +14,18 @@ namespace QuietTime
             InitializeComponent();
 
             DataContext = vm;
+        }
+
+        // Minimize to system tray when application is closed.
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            // setting cancel to true will cancel the close request
+            // so the application is not closed
+            e.Cancel = true;
+
+            this.Hide();
+
+            base.OnClosing(e);
         }
     }
 }
