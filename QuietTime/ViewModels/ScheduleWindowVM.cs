3fdc67599e57220d2c2a743fae2c90985d496468
Jack Kendall <jkendall3096@gmail.com>
2021-06-21 12:30:00 +0100

    Add rudimentary way to add schedules

--- QuietTime/ViewModels/ScheduleWindowVM.cs
+++ QuietTime/ViewModels/ScheduleWindowVM.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using QuietTime.Models;
 using System;
 using System.Collections.Generic;
@@ -18,8 +19,64 @@ namespace QuietTime.ViewModels
             set { _schedules = value; }
         }
 
+        private DateTime _start;
+        public DateTime Start
+        {
+            get { return _start; }
+            set { SetProperty(ref _start, value); }
+        }
+
+        private DateTime _end;
+        public DateTime End
+        {
+            get { return _end; }
+            set { SetProperty(ref _end, value); }
+        }
+
+        private int _volumeDuring;
+
+        public int VolumeDuring
+        {
+            get { return _volumeDuring; }
+            set { SetProperty(ref _volumeDuring, value); }
+        }
+
+        private int _volumeAfter;
+
+        public int VolumeAfter
+        {
+            get { return _volumeAfter; }
+            set { SetProperty(ref _volumeAfter, value); }
+        }
+
+        private RelayCommand _addSchedule;
+        public RelayCommand AddSchedule
+        {
+            get { return _addSchedule; }
+            set { SetProperty(ref _addSchedule, value); }
+        }
+
         public ScheduleWindowVM()
         {
+            _addSchedule = new RelayCommand(Add);
+
+            foreach (var item in Schedule.GetSchedules())
+            {
+                Schedules.Add(item);
+            }
+        }
+
+        private void Add()
+        {
+            var schedule = new Schedule(
+                TimeOnly.FromDateTime(Start), 
+                TimeOnly.FromDateTime(End), 
+                VolumeDuring, 
+                VolumeAfter);
+
+            Schedule.AddSchedule(schedule);
+
+            Schedules.Clear();
             foreach (var item in Schedule.GetSchedules())
             {
                 Schedules.Add(item);
