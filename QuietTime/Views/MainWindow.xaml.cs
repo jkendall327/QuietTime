f2401f5c3e97eb11cba5638511b6464613ddc1e5
Jack Kendall <jkendall3096@gmail.com>
2021-06-25 19:45:13 +0100

    Break scale value dependency object into own class

--- QuietTime/Views/MainWindow.xaml.cs
+++ QuietTime/Views/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace QuietTime
         /// <param name="svm">The viewmodel for the <see cref="ScheduleWindow"/> that can be opened from this window.</param>
         /// <param name="logger">Logging framework for this class.</param>
         /// <param name="notifications">Notification service for this class.</param>
+        /// <param name="settingsVM">Viewmodel for the <see cref="SettingsWindow"/> that can be opened from this window.</param>
         public MainWindow(MainWindowVM vm, ScheduleWindowVM svm, ILogger<MainWindow> logger, NotificationService notifications, SettingsWindowVM settingsVM)
         {
             InitializeComponent();
@@ -42,54 +43,13 @@ namespace QuietTime
             _settingsVM = settingsVM;
         }
 
-        /*
-         * Source for scaling UI: https://www.inchoatethoughts.com/scaling-your-user-interface-in-a-wpf-application
-         * I don't fully understand this, I'm not sure I want to fully understand this, but it seems to work OK.
-         */
+        // these all make the UI auto-scale when the window resizes
+        private static readonly ScaleValueHelper<MainWindow> _scaleHelper = new();
+        public readonly DependencyProperty ScaleValueProperty = _scaleHelper.Get();
 
         private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            CalculateScale();
-        }
-
-        private void CalculateScale()
-        {
-            double yScale = ActualHeight / 400.0d;
-            double xScale = ActualWidth / 500.0d;
-            double value = Math.Min(xScale, yScale);
-            ScaleValue = (double)OnCoerceScaleValue(MainGrid, value);
-        }
-
-        #region ScaleValue Depdency Property
-
-        public static readonly DependencyProperty ScaleValueProperty = DependencyProperty.Register(
-            "ScaleValue", typeof(double), typeof(MainWindow),
-            new UIPropertyMetadata(
-                1.0,
-                new PropertyChangedCallback(OnScaleValueChanged),
-                new CoerceValueCallback(OnCoerceScaleValue)));
-
-        private static object OnCoerceScaleValue(DependencyObject o, object value)
-        {
-            if (o is MainWindow mainWindow)
-            {
-                return mainWindow.OnCoerceScaleValue((double)value);
-            }
-
-            return value;
-        }
-
-        private static void OnScaleValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-        {
-
-        }
-
-        protected virtual double OnCoerceScaleValue(double value)
-        {
-            if (double.IsNaN(value))
-                return 1.0d;
-
-            return Math.Max(0.1, value);
+            ScaleValue = _scaleHelper.CalculateScale(ActualHeight, ActualWidth, MainGrid);
         }
 
         public double ScaleValue
@@ -98,7 +58,7 @@ namespace QuietTime
             set => SetValue(ScaleValueProperty, value);
         }
 
-        #endregion
+        // so we close to system tray by default
 
         protected override void OnClosing(CancelEventArgs e)
         {
@@ -107,7 +67,7 @@ namespace QuietTime
             if (e.Cancel)
             {
                 _notifications.SendNotification("Window closed",
-                    "QuietTime is still running in the system tray. You can re-open the main window or close the app completely from there.",
+                    "QuietTime is still running in the system tray. You can open the main window or close the app completely from there.",
                     NotificationService.MessageLevel.Information);
 
                 _logger.LogInformation(EventIds.AppClosingCancelled, "App sent to system tray.");
@@ -134,6 +94,8 @@ namespace QuietTime
             _traybarClosing = true;
             this.Close();
         }
+
+        // opening other windows
 
         private void Button_AddSchedule_Click(object sender, RoutedEventArgs e)
         {
