2b079ab6282eff690ebd42545bfa740d065388d5
Jack Kendall <jkendall3096@gmail.com>
2021-07-12 08:52:32 +0100

    Merge

--- QuietTime/ViewModels/MainPageVM.cs
+++ QuietTime/ViewModels/MainPageVM.cs
@@ -43,7 +43,23 @@ namespace QuietTime.ViewModels
                 audio.SwitchLock(NewMaxVolume);
             }
 
-            ChangeVolumeCommand = new RelayCommand<string>((amount) => ChangeNewMaxVolume(int.Parse(amount)));
+            TestCommand = new((val) =>
+            {
+                int amount = val switch
+                {
+                    VolumeAmounts.SmallIncrease => 5,
+                    VolumeAmounts.LargeIncrease => 15,
+                    VolumeAmounts.SmallDecrease => -5,
+                    VolumeAmounts.LargeDecrease => -15,
+                    _ => 0
+                };
+
+                ChangeNewMaxVolume(amount);
+            });
+
+            // todo: parameterize this command
+            IncreaseVolume = new RelayCommand(() => ChangeNewMaxVolume(5));
+            DecreaseVolume = new RelayCommand(() => ChangeNewMaxVolume(-5));
 
             // delta is how much mousewheel was moved
             // divide by ten because it's too big otherwise
